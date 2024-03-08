67b15c291cdf1b9f841b27edaf00cef16a601f4a
pritamkumarbishwas <pritamkumarbishwas85@gmail.com>
2024-03-08 22:51:30 +0530

    weather app

--- src/WeatherCard.jsx
+++ src/WeatherCard.jsx
@@ -5,7 +5,7 @@ const WeatherCard = ({ data }) => {
     const { temp_c, humidity, condition, wind_kph } = data.current;
 
     return (
-        <div className="weather-card">
+        <div >
             <div className="weather-info">
                 <div className="weather-info-item">
                     <div className="card">
